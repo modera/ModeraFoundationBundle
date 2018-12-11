f3d3b522874c4d95ef74107e9f63375309bc3f8e
Sergei Vizel <sergei.vizel@modera.net>
2018-12-11 11:04:54 +0200

    BackgroundProcess

---
+++ Process/BackgroundProcess.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modera\FoundationBundle\Process;
+
+use Symfony\Component\Process\Process;
+
+/**
+ * @author    Sergei Vizel <sergei.vizel@modera.org>
+ * @copyright 2018 Modera Foundation
+ */
+class BackgroundProcess extends Process
+{
+    /**
+     * @inheritdoc
+     */
+    public function start(callable $callback = null)
+    {
+        static::prepare($this);
+
+        return parent::start($callback);
+    }
+
+    /**
+     * @param Process $process
+     */
+    public static function prepare(Process $process)
+    {
+        $commandline = $process->getCommandLine();
+
+        if (substr(strtoupper(PHP_OS), 0, 3) === 'WIN') {
+            $process->setCommandLine('START /b "" ' . $commandline);
+        } else {
+            $process->setCommandLine('nohup ' . $commandline);
+        }
+    }
+}
